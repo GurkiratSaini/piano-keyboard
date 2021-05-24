0eece9a383db1da5c9a4ca1588dd64fedd43d7fd
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-23 20:48:24 -0400

    added functionality to record and play back songs

--- public/script.js
+++ public/script.js
@@ -1,11 +1,26 @@
 const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
 
+let recordingStartTime;
+let songNotes;
+
+const recordButton = document.querySelector('.record-button');
+const playButton = document.querySelector('.play-button');
+const saveButton = document.querySelector('.save-button');
 const keys = document.querySelectorAll('.key');
 const whiteKeys = document.querySelectorAll('.key.white');
 const blackKeys = document.querySelectorAll('.key.black');
 
+const keyMap = [...keys].reduce((map, key) => {
+    map[key.dataset.note] = key;
+    return map;
+}, {});
+
 keys.forEach(elem => elem.addEventListener('click', () => playNote(elem)));
+
+recordButton.addEventListener('click', toggleRecording);
+saveButton.addEventListener('click', saveSong);
+playButton.addEventListener('click', playSong);
 
 document.addEventListener('keydown', elem => {
     if (elem.repeat) return;
@@ -16,9 +31,47 @@ document.addEventListener('keydown', elem => {
 
     if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex]);
     if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex]);
-})
+});
+
+function toggleRecording() {
+    recordButton.classList.toggle('active');
+
+    if (isRecording()) {
+        startRecording();
+    } else {
+        stopRecording();
+    }
+}
+
+function isRecording() {
+    return recordButton != null && recordButton.classList.contains('active');
+}
+
+function startRecording() {
+    recordingStartTime = Date.now();
+    songNotes = [];
+    playButton.classList.remove('show');
+    saveButton.classList.remove('show');
+}
+
+function stopRecording() {
+    playSong();
+    playButton.classList.add('show');
+    saveButton.classList.add('show');
+}
+
+function playSong() {
+    console.log(songNotes);
+    if (songNotes.length === 0) return;
+    songNotes.forEach(note => {
+        setTimeout(() => {
+            playNote(keyMap[note.key])
+        }, note.startTime)
+    })
+}
 
 function playNote(elem) {
+    if (isRecording()) recordNote(elem.dataset.note);
     const audioFile = document.getElementById(elem.dataset.note);
     audioFile.currentTime = 0;
     audioFile.play();
@@ -26,4 +79,15 @@ function playNote(elem) {
     audioFile.addEventListener('ended', () => {
         elem.classList.remove('active');
     });
+}
+
+function recordNote(note) {
+    songNotes.push({
+        key: note,
+        startTime: Date.now() - recordingStartTime
+    })
+}
+
+function saveSong() {
+
 }
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('handlebars', exphbs());
@@ -13,5 +15,9 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.post('/songs', (req, res) => {
+    req.body.songNotes
+})
 
 app.listen(PORT, () => console.log(`App now listening on port ${PORT}`));
